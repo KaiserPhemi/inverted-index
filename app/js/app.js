1e9ab737f19467f9fa78b699c05a8070a34e5544
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-70.local>
2017-03-10 19:38:23 +0100

    class methods created

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,23 @@
 
 	class InvertedIndex{
 
+		/*Reads the data from the file being uploaded*/
+		readFile(){}
 
+		/*Ensures all the documents in a particular file is valid*/
+		validateFile(){}
+		
+		/*Strips out special characters from documents to be indexed*/
+		tokenize(){}
+		
+		/*Creates the index for documents*/
+		createIndex(){}
+
+		/*Get’s indices created for particular files*/
+		getIndex(){}
+
+		/*Searches through one or more indices for words*/
+		searchIndex(){}
 
 	}
 })();
