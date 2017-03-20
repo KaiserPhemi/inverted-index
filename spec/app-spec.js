425f179b0a04638f789489be4725310bce0e6c7b
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-03-20 23:00:12 +0100

    test file modified to include input cases

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,3 +1,10 @@
 {
+	const test = require('..app/js/app.js');
+
+	describe('Checks for input', () => {
+  		it('', () => {
+    		expect(test.readFile(book.json)).toEqual('Hello world!');
+  });
+});
 	
 }
