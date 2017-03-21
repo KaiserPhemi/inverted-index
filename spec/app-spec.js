4627c597504b98def60164a849f79780dc2fb8ae
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-03-21 07:56:54 +0100

    spec file modified

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,22 +1,26 @@
 {
+	/* Grab necessary dependencies */
 	const InvertedIndex = require('../app/js/app.js'),
-		  invIndexInstance = new InvertedIndex();
+		  invIndexInstance = new InvertedIndex(),
+		  badFormat 	= require('books/bad-format.json'),
+		  empty			= require('books/empty.json'),
+		  validBook		= require('books/books.json');
 
 	describe('Checks for valid input file', () => {
   		
   		it('Should return true for valid json file', () => {
-    		expect(invIndexInstance.validateFile(book.json)).toBe(true);
+    		expect(invIndexInstance.validateFile(validBook)).toBe(true);
   		});
 
   		it('Should return true for valid fields', () => {
-  			expect(invIndexInstance.validateFile('[{"title":"Harry Porter", "text":"Voldemort"}]')).toBe(true);
+  			expect(invIndexInstance.validateFile(empty)).toBe(true);
   		});
 	});
 
 	describe('Checks for invalid input file', () => {
   		
   		it('Should return false for invalid file', () => {
-    		expect(invIndexInstance.validateFile(fakebook.json)).toBe(false);
+    		expect(invIndexInstance.validateFile(badFormat)).toBe(false);
   		});
 	});
 	
