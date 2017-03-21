9ee45f837ab2f713bc632326d6d266fa3e54be1a
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-03-21 01:49:34 +0100

    edge cases added

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,17 +1,22 @@
 {
-	const test = require('../app/js/app.js');
+	const InvertedIndex = require('../app/js/app.js'),
+		  invIndexInstance = new InvertedIndex();
 
 	describe('Checks for valid input file', () => {
   		
   		it('Should return true for valid json file', () => {
-    		expect(test.validateFile(book.json)).toBe(true);
+    		expect(invIndexInstance.validateFile(book.json)).toBe(true);
+  		});
+
+  		it('Should return true for valid fields', () => {
+  			expect(invIndexInstance.validateFile('[{"title":"Harry Porter", "text":"Voldemort"}]')).toBe(true);
   		});
 	});
 
 	describe('Checks for invalid input file', () => {
   		
   		it('Should return false for invalid file', () => {
-    		expect(test.validateFile(fakebook.json)).toBe(false);
+    		expect(invIndexInstance.validateFile(fakebook.json)).toBe(false);
   		});
 	});
 	
