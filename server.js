09bd2d0ad92c7f2ca556949a798e10733eaabd5c
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-03-24 15:16:48 +0100

    Server code refactored

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 {
 	/* Grab required dependencies */
-	require('dotenv').config();
-	const express 	= require('express'),
-		  path 		= require('path'),
-		  app 		= express(),
-		  port 		= 7000; 
+	const connect	= require('connect'),
+		  serve		= require('serve-static')
+		  port 		= process.env.PORT || 7000; 
 
-	app.get('/', (request, response) => {
-	    response.sendFile(path.join(__dirname, 'app/index.html'));
+	connect().use(serve('./app')).listen(port, () => {
+		console.log(`Server running on port ${port}`);
 	});
-
-	/* Listening on port */
-	app.listen(process.env.PORT || port, ()=>{
-	    console.log(`App listening on port ${port}`);
-	})
 }
