84dc98449c86c46a04c033dfcc8ad508f6717cb3
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-03-28 17:58:50 +0100

    upload method added to module

--- app/js/mainModule.js
+++ app/js/mainModule.js
@@ -1,28 +1,41 @@
 {
+  const invIndex = new InvertedIndex();
   const MainController = ($scope) => {
-    const invIndex = new InvertedIndex();
     $scope.uploads = () => {
       invIndex.readFile();
-      $scope.uploadFile = () => {
-       let uploadedFiles = [];
-       if (window.File && window.FileReader && window.FileList
-        && window.Blob) {
-        let fileUpload = document.getElementsByClassName('file-upload')[0];
-        if (invIndex.validateFile(fileUpload)) {
-          uploadedFiles.push(fileUpload);
-        }
-      } else {
-        return 'The File APIs are not fully supported in this browser.';
-        }
-      }
     };
+    $scope.message = 'Default';
+    $scope.messageBox = getElementById('msg-box')[0];
+    $scope.feedback = (args) => {     
+      switch(args){
+        case 1:
+          message = 'Valid file. Upload successful';
+          messageBox.className += ' alert-success';
+        case 2:
+          message = 'Invalid JSON file';
+          messageBox.className += ' alert-danger';
+      }
+
+    }
   };
 
-  const Directive = () => {
-
-  };
+  const fileUpload = () => {
+    return {
+        scope: true,        //create a new scope
+        link: function (scope, el, attrs) {
+            el.bind('change', function (event) {
+                var files = event.target.files;
+                //iterate files since 'multiple' may be specified on the element
+                for (var i = 0;i <files.length;i++) {
+                    //emit event upward
+                    scope.$emit("fileSelected", { file: files[i] });
+                }                                       
+            });
+        }
+    };
+};
 
   angular.module('mainApp', [])
   .controller('MainController', MainController)
-  .directive('Directive', Directive);
+  .directive('fileUpload', fileUpload);
 }
