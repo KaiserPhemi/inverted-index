fb7d5fe893b58a0eca3a3fab986c7363f6e535a5
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-03-30 19:57:39 +0100

    Code refactored

--- app/js/mainModule.js
+++ app/js/mainModule.js
@@ -1,42 +1,33 @@
-{ 
-  
-  const MainController = ($scope) => {
-    const invIndex = new InvertedIndex();
-    $scope.uploads = () => {
-      invIndex.readFile();
-    };
-    $scope.message = '';
-    $scope.searchResult =
-
-   
+const MainController = ($scope) => {
+  $scope.invIndex = new InvertedIndex();
+  $scope.uploads = () => {};
+  $scope.message = '';
+  $scope.searchResult = {};
+  $scope.fileUpload = () => {
+  return {
+      scope: true,        //create a new scope
+      link: function (scope, el, attrs) {
+          el.bind('change', function (event) {
+              var files = event.target.files;
+              //iterate files since 'multiple' may be specified on the element
+              for (var i = 0;i <files.length;i++) {
+                  //emit event upward
+                  scope.$emit("fileSelected", { file: files[i] });
+              }                                       
+          });
+      }
   };
-
-  const fileUpload = () => {
-    return {
-        scope: true,        //create a new scope
-        link: function (scope, el, attrs) {
-            el.bind('change', function (event) {
-                var files = event.target.files;
-                //iterate files since 'multiple' may be specified on the element
-                for (var i = 0;i <files.length;i++) {
-                    //emit event upward
-                    scope.$emit("fileSelected", { file: files[i] });
-                }                                       
-            });
-        }
-    };
 };
 
 // Document ready.
 document.addEventListener('DOMContentLoaded', () => {
-  // Attach file upload listener
-  document.getElementById('json-file')
-    .addEventListener('change', function createFile() {
-      if (this.files[0]) {
-        angular.element(this).scope().createIndex(this.files[0]);
-      }
-    });
+// Attach file upload listener
+document.getElementById('json-file')
+  .addEventListener('change', function createFile() {
+    if (this.files[0]) {
+      angular.element(this).scope().createIndex(this.files[0]);
+    }
+  });
 });
-  angular.module('mainApp', [])
-  .controller('MainController', MainController);
-}
+angular.module('mainApp', [])
+.controller('MainController', MainController);
