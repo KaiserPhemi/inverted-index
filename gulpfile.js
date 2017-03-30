9355395d38f13d1e1a50719b0f47439026321337
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-03-30 20:00:48 +0100

    code refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,29 @@
 /* eslint-disable */
-{
-  /**
-   * Gulpfile that watches for file changes and reloads page
-   * @type {[type]}
-   */
-  const gulp = require('gulp'),
-    pageSync = require('browser-sync').create(),
-    watch = require('gulp-watch');
+/**
+ * Gulpfile that watches for file changes and reloads page
+ * @type {[type]}
+ */
+const gulp = require('gulp'),
+  pageSync = require('browser-sync').create(),
+  watch = require('gulp-watch');
 
-  /* Create a default task */
-  gulp.task('default', ['watch']);
+/* Create a default task */
+gulp.task('default', ['watch']);
 
-  /** Task to reload for every change */
-  gulp.task('watch', ['sync'], () => {
-    gulp.watch('app/*html', pageSync.reload())
-        .watch('app/js/*.js', pageSync.reload())
-        .watch('app/css/*.css', pageSync.reload());
+/** Task to reload for every change */
+gulp.task('watch', ['sync'], () => {
+  gulp.watch('app/*html', pageSync.reload())
+      .watch('app/js/*.js', pageSync.reload())
+      .watch('app/css/*.css', pageSync.reload());
+});
+
+/* Create a task to watch for file changes */
+gulp.task('sync', () => {
+  pageSync.init({
+    server: {
+      baseDir: 'app/',
+      index: 'index.html'
+    },
+    port: process.env.PORT || 9000
   });
-
-  /* Create a task to watch for file changes */
-  gulp.task('sync', () => {
-    pageSync.init({
-      server: {
-        baseDir: 'app/',
-        index: 'index.html'
-      },
-      port: process.env.PORT || 9000
-    });
-  });
-}
+});
