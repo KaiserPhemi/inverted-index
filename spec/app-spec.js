63436f013caf8ade718e54a6b754de978e304756
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-04-02 13:58:38 +0100

    new test case included

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -54,10 +54,18 @@ describe('tokenize method', () => {
  * Test cases for createIndex method
  */
 describe('createIndex method', () => {
+  it('should return `true` if index is created', () => {
+    expect(invIndexInstance.createIndex('other-book', otherBook))
+    .toEqual(true);
+  });
+});
+/**
+ * Test case for getIndex method
+ */
+describe('getIndex method', () => {
   it(`should return index of words and 
     number of books in a json file`, () => {
-    expect(invIndexInstance.createIndex('other-book', otherBook))
-    .toEqual({ words: { alice: [0],
+    expect().toEqual({ words: { alice: [0],
       in: [0],
       wonderland: [0],
       a: [0],
