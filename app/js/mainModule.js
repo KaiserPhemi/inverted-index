0b1fa9c5d9dae50f2242af587bf89809d4de942a
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-04-02 13:58:00 +0100

    angular module modified to include search index function

--- app/js/mainModule.js
+++ app/js/mainModule.js
@@ -1,48 +1,94 @@
 /**
+ * Instance of class created
+ * @type Object
+ */
+const invIndex = new InvertedIndex();
+/**
  * Controller handling basic features of app
  * @param  {Object} $scope Binds the view with the controller
- * @return {[type]}        [description]
+ * @return {Object}        [description]
  */
 const MainController = ($scope) => {
-  const invIndex = new InvertedIndex();
-  $scope.message = 'default';
+  $scope.message = '';
+  $scope.fileNames = [];
   $scope.searchResult = {};
-  $scope.fileUpload = (args) => {
-    let allFiles = args.target.files;
-    console.table(allFiles);
-    console.table(allFiles.length);
-    /*let fileInput = document.querySelector('#all-files').addEventListener('change', () => console.log(fileInput.files));*/
-    /*let allFilesUploaded = fileInput.files;*/
-/*    console.log(fileInput.files);
-*/    /*let uploads = allFilesUploaded.length,
-    count = 0;
-    while(count < uploads){
-      let singleFile = allFilesUploaded[uploads];
-      /*console.log(allFilesUploaded.name);*/
-      /*count++;*/
-  /*  }*/
+  $scope.fileObjects = {};
+  $scope.allIndicies = {};
+  $scope.fileUpload = (event) => {
+    const allUploads = event.target;
+    for (let count = 0; count < allUploads.files.length; count += 1) {
+      invIndex.readFile(allUploads.files[count]).then((content) => {
+        if (JSON.parse(content)) {
+          $scope.fileObjects[allUploads.files[count].name] = JSON.parse(content);
+          if ((!$scope.fileNames
+            .includes(allUploads.files[count].name))) {
+            $scope.$apply($scope.fileNames.push(allUploads.files[count].name));
+          }
+        } else {
+          $scope.message = `${allUploads.files[count].name} 
+            is not a valid json file.`;
+        }
+      });
+    }
+    return $scope.fileObjects;
   };
-  document.querySelector('#all-files').addEventListener('change', $scope.fileUpload);
+  document.querySelector('#all-files')
+  .addEventListener('change', $scope.fileUpload);
 
-  $scope.createIndex = () => {
-    $scope.tableMsg = '';
+  $scope.createIndex = (selectFile) => {
+    const fileContent = $scope.fileObjects[selectFile];
+    let fileName = selectFile;
+    if (invIndex.createIndex(fileName, fileContent)) {
+      const indexed = invIndex.getIndex(fileName);
+      let uniqueWords = Object.keys(indexed);
+      let numOfBook = $scope.getNumOfBooks(fileName);
+
+      $scope.allIndicies[fileName] = {
+        uniqueWords,
+        numOfBook,
+        indexed
+      };
+    }
+    return true;
   };
+  /**
+   * Function to get book count in a file
+   * @param  {Object} fileName [description]
+   * @return {[type]}          [description]
+   */
+  $scope.getNumOfBooks = (fileName) => {
+    let fileContent = $scope.fileObjects[fileName],
+    arr = [];
+    for (let i = 0; i < fileContent.length; i += 1) {
+      arr.push(i);
+    }
+    return arr;
+  };
+  /**
+   * Function to search index
+   */
+  $scope.searchIndex = (term) => {
+    const searched = invIndex.searchIndex(terms);
+
+  };
+};
+/**
+ * Function to handle icon display inside table
+ * @return {String} String referencing and icon name.
+ */
+const rowIcon = () => {
+  return (input, arr) => {
+    if (arr.includes(input)) {
+      return 'cross';
+    } else {
+      return 'dash';
+      }
+  }
 };
 MainController.$inject = ['$scope'];
-/**
- * Controller handling table display
- * @param  {Object} $scope Binds the view with the controller
- * @return {[type]}        [description]
- */
-const TableController = ($scope) => {
-  $scope.table = null;
-  $scope.tableMsg = 'No index created';
-};
-TableController.$inject = ['$scope'];
-
 /**
  * Module declaration
  */
 angular.module('mainApp', [])
 .controller('MainController', MainController)
-.controller('TableController', TableController);
+.filter('imgCheck', rowIcon);
