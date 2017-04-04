62932006cc3e83d275afa33a7183d54ae3f51f2b
Oluwafemi Akinwa <oluwafemi.akinwa@andela.com>
2017-04-04 18:51:56 +0100

    server file modified

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-{
-	/* Grab required dependencies */
-	const connect	= require('connect'),
-		  serve		= require('serve-static')
-		  port 		= process.env.PORT || 7000; 
+/**
+ * Dependencies added
+ * @type {Object}
+ */
+const connect = require('connect'),
+    serve   = require('serve-static')
+    port    = process.env.PORT || 7000; 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	app.get('/', (request, response) => {
-	    response.sendFile(path.join(__dirname, 'app/index.html'));
-=======
-	app.get('*', (request, response) => {
-	    response.sendFile(path.join(__dirname, 'lib/index.html'));
->>>>>>> node modules installed
-=======
-	app.get('*', (request, response) => {
-	    response.sendFile(path.join(__dirname, 'lib/index.html'));
->>>>>>> e0d1b649ed9beb3cbf30b8676d9ec0bd9eff3669
-=======
-	connect().use(serve('./app')).listen(port, () => {
-		console.log(`Server running on port ${port}`);
->>>>>>> development
-	});
-}
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, 'app/index.html'));
+
+connect().use(serve('./app')).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
